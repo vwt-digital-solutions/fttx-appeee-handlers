900c8d4c317b9f9057ea285b8a8e61fa1c763590
Daimy van Rhenen <d.vanrhenen@vwt.digital>
2021-03-19 15:44:14 +0100

    Make sure Gobits in json format. DAT-6018

--- functions/get_images/publish_service.py
+++ functions/get_images/publish_service.py
@@ -19,7 +19,7 @@ class PublishService:
     @retry(tries=5, delay=5, backoff=2, logger=None)
     def publish_message(self, form_entry):
         gobits = Gobits.from_context(context=self.context)
-        message_to_publish = {"gobits": [gobits], "appee_survey": form_entry}
+        message_to_publish = {"gobits": [gobits.to_json()], "appee_survey": form_entry}
         logging.info(f"Publishing message on topic {self._topic_name}")
         self._publisher.publish(
             self._topic_name, bytes(json.dumps(message_to_publish).encode("utf-8"))
